c2b6e0137a5fb9576b2cfe35a5233c4e4414465e
JeroenUijtdewilligen <0988919@hr.nl>
2022-06-22 15:38:41 +0200

    Projectspage improved

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -5,18 +5,15 @@ const ProjectsPage: FC = () => {
   return (
     <div>
       <Navbar />
-      <section
-        id="Intro"
-        className="relative md:bg-gradient-to-t md:from-[#c0f56a] md:to-[#b5deff]"
-      >
-        <div className="grid grid-cols-2">
+      <section id="Intro" className="relative">
+        <div className="sm:grid sm:grid-cols-2">
           <div>
             <img src="/images/JeroenUijt.jpeg" alt="Ikzeluf" />
           </div>
-          <div className="flex justify-center p-4">
+          <div className="flex justify-center bg-gradient-to-t from-[#c0f56a] to-[#b5deff] p-4">
             <div className="self-center rounded-lg bg-gray-700 bg-opacity-50 p-2 text-center text-white">
               <h1 className="text-4xl font-semibold">
-                Hieronder een lijt met al mijn projecten!
+                Hieronder een lijst met al mijn projecten!
               </h1>
               <h4>School & Persoonlijke projecten</h4>
             </div>
