db5c4283f773b5cbd7efb21f5a49738b5bc4bbf2
JeroenUijtdewilligen <0988919@hr.nl>
2022-06-22 15:25:34 +0200

    made it so that the image and text are under eachother on mobile

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,15 +6,12 @@ const HomePage: FC = () => {
   return (
     <div>
       <Navbar />
-      <section
-        id="Intro"
-        className="relative md:bg-gradient-to-t md:from-[#c0f56a] md:to-[#b5deff]"
-      >
-        <div className="grid grid-cols-2">
+      <section id="Intro" className="relative">
+        <div className="md:grid md:grid-cols-2">
           <div>
             <img src="/images/JeroenUijt.jpeg" alt="Ikzeluf" />
           </div>
-          <div className="flex justify-center p-4">
+          <div className="flex justify-center bg-gradient-to-t from-[#c0f56a] to-[#b5deff] p-4">
             <div className="self-center rounded-lg bg-gray-700 bg-opacity-50 p-2 text-center text-white">
               <h1 className="text-4xl font-semibold">Hoi, ik ben Jeroen!</h1>
               <h4>Derdejaars student Informatica</h4>
