b69a96fb2723572e8a48e687e43a95df7db1f580
Jeroen <jeroenuijtdewilligen@gmail.com>
2025-09-15 23:19:48 +0200

    overflow fix test

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,12 +9,12 @@ const HomePage: FC = () => {
       <section id="Intro" className="relative">
         <div className="sm:grid sm:grid-cols-2">
           <div className="h-52">
-            <img className="object-cover" src="/images/JeroenUijt.jpeg" alt="Ikzeluf" />
+            <img className="object-cover overflow-hidden" src="/images/JeroenUijt.jpeg" alt="Ikzeluf" />
           </div>
           <div className="flex justify-center bg-gradient-to-t from-[#c0f56a] to-[#b5deff] p-4">
             <div className="self-center rounded-lg bg-gray-700 bg-opacity-50 p-2 text-center text-white">
               <h1 className="text-4xl font-semibold">Hoi, ik ben Jeroen!</h1>
-              <h4>Vierdejaars student Informatica</h4>
+              <h4>Software developer</h4>
               <SocialIcons />
             </div>
           </div>
