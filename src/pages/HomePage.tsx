af3b0066e3c0d48f3c7167023df1411f690f902a
Jeroen <jeroenuijtdewilligen@gmail.com>
2025-09-15 23:57:55 +0200

    new color added

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,7 +11,7 @@ const HomePage: FC = () => {
           <div className="overflow-hidden">
             <img className="object-cover w-full" src="/images/JeroenUijt.jpeg" alt="Ikzeluf" />
           </div>
-          <div className="flex justify-center bg-gradient-to-t from-[#1f1e1e] to-[#e1c18e] p-4">
+          <div className="flex justify-center bg-gradient-to-t from-[#1f1e1e] via-[#a889bc] to-[#e1c18e] p-4">
             <div className="self-center rounded-lg bg-gray-700 bg-opacity-50 p-2 text-center text-white">
               <h1 className="text-4xl font-semibold">Hoi, ik ben Jeroen!</h1>
               <h4>Software developer</h4>
